5c86ea3a89dd7e9ed86ad8a1ecc07299ba155f64
Vera Peeters <vera.peeters@thomasmore.be>
2024-08-11 20:28:40 +0200

    chapter 1: ProductNote

--- src/data/data.js
+++ src/data/data.js
@@ -16,9 +16,10 @@ ATTENTION!!!!
 
 export const PRODUCTS_DATA = [
     {id: 1, name: "cola", price: 1},
-    {id: 2, name: "water", price: 1},
+    {id: 2, name: "water", price: 1, note: "plat of bruisend"},
     {id: 3, name: "bier", price: 1, size: 25},
     {id: 4, name: "bier", price: 1, size: 33},
-    {id: 5, name: "wijn", price: 2},
+    {id: 5, name: "wijn", price: 2, note:"rood, wit of rosé"},
     {id: 6, name: "cava", price: 3}
 ];
+
