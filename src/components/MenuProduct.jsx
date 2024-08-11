994db7c2ffabe419f014adf030c73009fb696bc6
Vera Peeters <vera.peeters@thomasmore.be>
2024-08-11 20:23:55 +0200

    chapter 1: extract component ProductSize

--- src/components/MenuProduct.jsx
+++ src/components/MenuProduct.jsx
@@ -1,13 +1,22 @@
+function ProductSize(props) {
+    const {product} = props;
+    if (!product?.size) return;
+
+    return (
+        <span style={{fontSize: "small", color: "blue", "margin-left": "3px"}}>
+            ({product.size}cl)
+        </span>
+    );
+}
 export function MenuProduct(props) {
     const {product} = props;
     if (!product?.name) return;
 
-    const size = product.size && ` (${product.size}cl)`;
     return (
         <div style={{margin: "2vw", fontSize: "larger", display: "flex"}}>
             <div style={{flex: 1}}>
                 {product.name}
-                <span style={{fontSize: "small", color: "blue"}}>{size}</span>
+                <ProductSize product={product}/>
             </div>
             <div style={{flex: 1}}>{product.price} &euro;</div>
         </div>
