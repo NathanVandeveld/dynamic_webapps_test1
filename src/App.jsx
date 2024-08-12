389caf285f08a80c4920cd7752d31ee20bcee248
Vera Peeters <vera.peeters@thomasmore.be>
2024-08-12 11:52:24 +0200

    chapter 2: use react-tabs
    (cherry picked from commit fcc2c012bb23f7b859480f5d8620a1e109a76e42)

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,25 @@
 import 'normalize.css';
+import 'react-tabs/style/react-tabs.css';
 import './App.css'
 import {PRODUCTS_DATA} from "./data/data.js";
 import {MenuCardPage} from "./pages/MenuCardPage.jsx";
 import {PicturesPage} from "./pages/PicturePage.jsx";
+import {Tab, TabList, TabPanel, Tabs} from "react-tabs";
 
 function App() {
     return (
-        <>
-            <MenuCardPage products={PRODUCTS_DATA}/>
-            <PicturesPage/>
-        </>
-    )
+        <Tabs>
+            <TabList>
+                <Tab>menu</Tab>
+                <Tab>pics</Tab>
+            </TabList>
+            <TabPanel>
+                <MenuCardPage products={PRODUCTS_DATA}/>
+            </TabPanel>
+            <TabPanel>
+                <PicturesPage/>
+            </TabPanel>
+        </Tabs>)
 }
 
 export default App
