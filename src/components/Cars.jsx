9c279f6d0241974e98b78becbccd14fcb9e42309
Vera Peeters <vera.peeters@thomasmore.be>
2024-08-13 19:12:02 +0200

    chapter 3: cars colors

--- src/components/Cars.jsx
+++ src/components/Cars.jsx
@@ -2,6 +2,25 @@ import {Card, Col} from "react-bootstrap";
 import {Section} from "./Section.jsx";
 import {SectionCard} from "./SectionCard.jsx";
 
+const COLORS_CAR_CSS = {
+    blauw: {backgroundColor: "blue", color: "white"},
+    geel: {backgroundColor: "yellow"},
+    zwart: {backgroundColor: "black", color: "white"},
+    wit: {backgroundColor: "white"},
+    rood: {backgroundColor: "red", color: "white"},
+    grijs: {backgroundColor: "grey", color: "white"},
+    groen: {backgroundColor: "green", color: "white"},
+};
+
+function CarColor(props) {
+    const {color} = props;
+
+    if (!color) return null;
+    return <div style={COLORS_CAR_CSS[color]}>
+        kleur: {color} &nbsp;
+    </div>;
+}
+
 function Car(props) {
     const {car} = props;
     return (
@@ -9,6 +28,9 @@ function Car(props) {
             {car.brand && <div>merk: {car.brand}</div>}
             {car.type && <div>type: {car.type}</div>}
             {car.color && <div>kleur: {car.color}</div>}
+            <div className="mt-auto">
+                <CarColor color={car.color}/>
+            </div>
         </SectionCard>
     );
 }
