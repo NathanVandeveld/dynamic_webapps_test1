20dc027997c0b21f6a49011e31366447b3e16742
Vera Peeters <vera.peeters@thomasmore.be>
2024-08-14 12:05:46 +0200

    chapter 3: wiki: render images
    (cherry picked from commit 0421944b215e2561bd7d412e1365b92563d61ada)

--- src/components/WikiText.jsx
+++ src/components/WikiText.jsx
@@ -3,7 +3,11 @@ import {Section} from "./Section.jsx";
 function WikiTextLine(props) {
     const {line} = props;
     if (line.startsWith("---")) return <hr/>;
-
+    if (line.startsWith("[[")) return (
+        <div className="mx-5">
+            <img src={`images/wiki/${line.substring(2)}`} className="w-100 h-100" alt=""/>
+        </div>
+    );
     return (
         <div>{line}</div>
     );
--- src/components/stories/WikiText.stories.jsx
+++ src/components/stories/WikiText.stories.jsx
@@ -7,12 +7,14 @@ const testData = {
         "Simple text",
         "Another sentence",
         "---",
+        "[[zaaien-800x450.jpg"
     ]
 }
 /** WikiText renders 1 wiki object.
  * * A wiki object has a (unique) pageNumber, a title and a text.
  * * The text property is an array of strings.
- * * when a string starts with ---, a line is rendered
+ * * when a string starts with ---, a line is rendered.
+ * * when a string starts with [[, the rest of line contains the filename of an image.
  */
 const meta = {
     component: WikiText,
