56e1b2ac211be20146770e3417ded7f989d60c33
Vera Peeters <vera.peeters@thomasmore.be>
2024-08-14 12:13:53 +0200

    chapter 3: wiki: refactor as a start to add italic
    (cherry picked from commit fc6a68b9db17bccc764b48ddcab4de6959d10c85)

--- src/components/WikiText.jsx
+++ src/components/WikiText.jsx
@@ -1,15 +1,29 @@
 import {Section} from "./Section.jsx";
 
+function WikiImage(props) {
+    const {imageName} = props;
+    return (
+        <div className="mx-5">
+            <img src={`images/wiki/${imageName}`} className="w-100 h-100" alt=""/>
+        </div>
+    );
+}
+
+function BoldAndItalicLineParts(props) {
+    const {line} = props;
+    const lineParts = line.split("*");
+    return (
+        <div>
+            {lineParts.map((p, i) => i % 2 ? <b key={i}>{p}</b> : p)}
+        </div>
+    );
+}
+
 function WikiTextLine(props) {
     const {line} = props;
     if (line.startsWith("---")) return <hr/>;
-    if (line.startsWith("[[")) return (
-        <div className="mx-5">
-            <img src={`images/wiki/${line.substring(2)}`} className="w-100 h-100" alt=""/>
-        </div>
-    );
-    const lineParts = line.split("*");
-    return <div>{lineParts.map((p, i) => i % 2 ? <b key={i}>{p}</b> : p)}</div>;
+    if (line.startsWith("[[")) return <WikiImage imageName={line.substring(2)}/>;
+    return <BoldAndItalicLineParts line={line}/>;
 }
 
 export function WikiText(props) {
