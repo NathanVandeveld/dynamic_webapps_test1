dfc4ad5d83812397eda9af2f67953d8c38b506fb
Vera Peeters <vera.peeters@thomasmore.be>
2024-08-14 12:16:34 +0200

    chapter 3: wiki: italic
    (cherry picked from commit 8319712a60b6049a9393da636fe1950f5c6946bb)

--- src/components/WikiText.jsx
+++ src/components/WikiText.jsx
@@ -9,12 +9,24 @@ function WikiImage(props) {
     );
 }
 
+function ItalicLineParts(props) {
+    const {line} = props;
+    const lineParts = line.split("_");
+    return (
+        <>
+            {lineParts.map((p, i) => i % 2 ? <i key={i}>{p}</i> : p)}
+        </>
+    );
+}
+
 function BoldAndItalicLineParts(props) {
     const {line} = props;
     const lineParts = line.split("*");
     return (
         <div>
-            {lineParts.map((p, i) => i % 2 ? <b key={i}>{p}</b> : p)}
+            {lineParts.map((p, i) => i % 2
+                ? <b key={i}>{p}</b>
+                : <ItalicLineParts line={p} key={i}/>)}
         </div>
     );
 }
--- src/components/stories/WikiText.stories.jsx
+++ src/components/stories/WikiText.stories.jsx
@@ -9,6 +9,7 @@ const testData = {
         "---",
         "[[zaaien-800x450.jpg",
         "text with a *bold* word.",
+        "text with a _italic_ word.",
     ]
 }
 /** WikiText renders 1 wiki object.
@@ -17,6 +18,8 @@ const testData = {
  * * when a string starts with ---, a line is rendered.
  * * when a string starts with [[, the rest of line contains the filename of an image.
  * * when a part of a string is enclosed between 2 * characters, that part will be rendered as bold.
+ * * when a part of a string is enclosed between 2 _ characters, that part will be rendered as italic.
+ * * bold and italic can not be combined.
  */
 const meta = {
     component: WikiText,
