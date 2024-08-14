a02a03fdacf6f194b964f1281494085c8012c473
Vera Peeters <vera.peeters@thomasmore.be>
2024-08-14 11:56:25 +0200

    chapter 3: wiki: WikiText step 3 (show pageNumber and text)
    (cherry picked from commit 84b39dee37e0c6664e15f97e9e63cc74229ad9af)

--- src/components/WikiText.jsx
+++ src/components/WikiText.jsx
@@ -1,10 +1,21 @@
 import {Section} from "./Section.jsx";
 
+function WikiTextLine(props) {
+    const {line} = props;
+    return (
+        <div>
+            {line}
+        </div>
+    );
+}
+
 export function WikiText(props) {
     const {wikitext} = props;
     return (
         <Section title={wikitext.title}>
-            <div>
+            <div className="text-center">
+                {wikitext.text.map((l, i) => <WikiTextLine line={l} key={i}/>)}
+                <footer>-{wikitext.pageNumber}-</footer>
             </div>
         </Section>
     )
