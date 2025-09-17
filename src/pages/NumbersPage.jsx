50cfd38706c6a50138055e3796bb0c104a17d9ba
Vera Peeters <vera.peeters@thomasmore.be>
2025-09-17 16:57:45 +0200

    chapter 3: numbers > 6 en numbers * 2

--- src/pages/NumbersPage.jsx
+++ src/pages/NumbersPage.jsx
@@ -4,8 +4,10 @@ export function NumbersPage(props) {
     const {numbers} = props
 
     return (
-        <div className="mx-3" >
+        <div className="mx-3">
             <Numbers numbers={numbers} title="alle getallen"/>
+            <Numbers numbers={numbers.filter(n => n > 6)} title="getallen>6"/>
+            <Numbers numbers={numbers.map(n => n * 2)} title="getallen*2"/>
         </div>
     );
 }
