d458bb10fcd7f12bf36a6ca329ccb61b775e8a83
Vera Peeters <vera.peeters@thomasmore.be>
2025-09-17 14:15:10 +0200

    chapter 3: cities

--- src/pages/PersonsPage.jsx
+++ src/pages/PersonsPage.jsx
@@ -12,6 +12,7 @@ function PersonAge(props) {
         </Col>
     );
 }
+
 function PersonAges(props) {
     const {persons, title} = props;
     const ages = persons.map(p => p.age);
@@ -23,6 +24,37 @@ function PersonAges(props) {
             <Container fluid>
                 <Row>
                     {uniqueAgesSorted.map(a => <PersonAge key={a} age={a} persons={persons}/>)}
+                </Row>
+            </Container>
+        </div>
+    );
+}
+
+function City(props) {
+    const {city} = props;
+    return (
+        <Col className="m-0" xs={12} sm={6} md={4} lg={3} xl={2} xxl={2}>
+            <Card className="m-1 p-2 shadow-sm text-center">
+                <h5>{city.city}</h5>
+                <div>{city.numberOfPersons}</div>
+            </Card>
+        </Col>
+    );
+}
+
+function Cities(props) {
+    const {persons, title} = props;
+    const cities = persons.map(p => p.city);
+    const uniqueCities = [...new Set(cities)];
+    const citiesWithNumberOfPersons = uniqueCities.map(c =>
+        ({city: c, numberOfPersons: persons.filter(p => p.city === c).length}));
+    const citiesSorted = citiesWithNumberOfPersons.toSorted((c1, c2) => c1.numberOfPersons - c2.numberOfPersons);
+    return (
+        <div className="mt-3 pb-2 rounded shadow-sm" style={{backgroundColor: "lavender"}}>
+            <h2 className="text-center">{title}</h2>
+            <Container fluid>
+                <Row>
+                    {citiesSorted.map(c => <City key={c.city} city={c}/>)}
                 </Row>
             </Container>
         </div>
@@ -41,6 +73,7 @@ export function PersonsPage(props) {
             <Persons persons={persons.toSorted((pl, pr) => pl.age - pr.age)}
                      title="sorteer op leeftijd"/>
             <PersonAges persons={persons} title="leeftijden"/>
+            <Cities persons={persons} title="steden"/>
         </div>
     );
 }
